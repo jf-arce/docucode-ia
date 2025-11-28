038b0cd64dd6c3cff4465f38eaefe2074e7e3351
jf-arce <josefranciscoarce21@gmail.com>
2025-11-27 23:11:41 -0300

    feat: add the EmptyWorkspace component with GitHub and new project options, and introduces the FileTextIcon and GitHubIcon components

--- src/components/EmptyWorkspace.tsx
+++ src/components/EmptyWorkspace.tsx
@@ -1,20 +1,39 @@
-import { FileText } from "lucide-react";
 import { NewProjectButton } from "@/components/NewProjectButton";
+import { Input } from "./ui/input";
+import { FileTextIcon, GitHubIcon } from "./Icons";
 
 export const EmptyWorkspace = () => {
 	return (
-		<div className="flex flex-col flex-1 items-center justify-center gap-6 mb-[70px]">
-			<div className="text-center flex flex-col gap-5">
-				<div>
-					<FileText className="mx-auto h-12 w-12 text-primary/50" />
-					<p className="mt-3 text-lg font-normal text-foreground">No document selected</p>
+		<div className="flex flex-1 items-center justify-center gap-6 mb-[70px]">
+			<div className="flex flex-col lg:flex-row gap-6 justify-between">
+				<div className="max-w-sm flex flex-col gap-4 bg-primary/5 p-6 rounded-lg border border-primary/10">
+					<div className="flex flex-col gap-2">
+						<GitHubIcon size={48} />
+						<h2 className="text-xl font-medium">Document your repository from GitHub</h2>
+						<p className="text-md text-muted-foreground">
+							Put your repository link and let DocuCode AI document your code
+						</p>
+					</div>
+					<Input placeholder="GitHub repository link" />
 				</div>
-				<p className="text-md text-muted-foreground">
-					Select an existing document or Create a new project to get started documenting your code.
-				</p>
-			</div>
-			<div>
-				<NewProjectButton />
+
+				<div className="flex items-center">
+					<p className="text-lg font-medium">or</p>
+				</div>
+
+				<div className="flex flex-col justify-between gap-6 max-w-sm bg-primary/5 p-6 rounded-lg border border-primary/10">
+					<div className="flex flex-col gap-2">
+						<FileTextIcon size={48} className="text-primary/50" />
+						<p className="text-xl font-medium text-foreground">Create a new project</p>
+
+						<p className="text-md text-muted-foreground">
+							Create a new project and then create a new document to start documenting your code.
+						</p>
+					</div>
+					<div>
+						<NewProjectButton />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
